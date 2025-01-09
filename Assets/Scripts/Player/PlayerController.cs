1144aaea87ed29898a1235500155fc512c33d152
Genofabio <fabio.genovese.00@gmail.com>
2025-01-09 04:57:41 +0100

    PlayerController migliorato
    Ora il player controller è di tipo rigidbody e non più character controller.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,33 +3,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController characterController;
 
-    // --- Movimento ---
-    [Header("Movement Settings")]
-    [SerializeField] private float movementSpeed;
+    [Header("Camera")]
+    [SerializeField] private GameObject cameraHolder;
 
-    private Vector2 movementInput;
+    [Header("Settings")]
+    [SerializeField] private float movementSpeed = 4f;
+    [SerializeField] private float rotationSensitivity = 0.3f;
+    [SerializeField] private float maxForce = 1f;
 
-    // --- Telecamera ---
-    [Header("Camera Settings")]
-    [SerializeField] private Camera playerCamera;
-    [SerializeField] private float lookSensityvityX = 30f;
-    [SerializeField] private float lookSensitivityY = 30f;
-    
-    private float cameraPitch = 0f;
-    private Vector2 lookInput;
+    private Rigidbody rb;
+    private Vector2 movementInput, lookInput;
+    private float verticalLookRotation;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked; // nasconde l'icona del cursore
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        movePlayer();
-        playerLook();
+        MovePlayer();
+    }
+
+    private void LateUpdate()
+    {
+        RotateCamera();
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -42,21 +42,34 @@ public class PlayerController : MonoBehaviour
         lookInput = context.ReadValue<Vector2>();
     }
 
-    public void movePlayer()
+    public void MovePlayer()
     {
-        Vector3 moveDirection = new Vector3(movementInput.x, 0, movementInput.y);
-        characterController.Move(transform.TransformDirection(moveDirection) * movementSpeed * Time.deltaTime);
+        // Calcola la velocità target
+        Vector3 currentVelocity = rb.linearVelocity;
+        Vector3 targetVelocity = new Vector3(movementInput.x, 0, movementInput.y) * movementSpeed;
+        targetVelocity = transform.TransformDirection(targetVelocity);
+
+        // Calcola il cambiamento di velocità richiesto
+        Vector3 velocityChange = (targetVelocity - currentVelocity);
+        velocityChange.y = 0;
+
+        // Limita la forza applicata
+        velocityChange = Vector3.ClampMagnitude(velocityChange, maxForce);
+
+        // Applica la forza
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
     }
 
-    public void playerLook()
+    public void RotateCamera()
     {
-        // Rotazione sull'asse X (su/gi�)
-        cameraPitch -= lookInput.y * lookSensitivityY * Time.deltaTime;
-        cameraPitch = Mathf.Clamp(cameraPitch, -80f, 80f);
-        playerCamera.transform.localRotation = Quaternion.Euler(cameraPitch, 0, 0);
+        // Rotazione orizzontale
+        transform.Rotate(Vector3.up * lookInput.x * rotationSensitivity);
 
-        // Rotazione sull'asse Y (destra/sinistra)
-        float yRotation = lookInput.x * lookSensityvityX * Time.deltaTime;
-        transform.Rotate(Vector3.up * yRotation);
+        // Rotazione verticale
+        verticalLookRotation -= lookInput.y * rotationSensitivity;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -85f, 85f);
+
+        // Applica la rotazione verticale
+        cameraHolder.transform.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);
     }
 }
