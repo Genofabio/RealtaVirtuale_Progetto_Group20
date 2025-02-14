f6ecdbb09f60d968e394f97e6168ee502eba5c42
Tommaso Querci <tommiquerci@gmail.com>
2025-02-14 07:55:48 +0100

    Facce

--- Assets/Assistente/Scripts/Assistant.cs
+++ Assets/Assistente/Scripts/Assistant.cs
@@ -10,6 +10,8 @@ public class Assistant : MonoBehaviour
     [SerializeField] Canvas screen;
     [SerializeField] private List<Sprite> images; 
     [SerializeField] private Image displayImage;
+    [SerializeField] private Image successImage;
+    [SerializeField] private Image failImage;
 
     [Header("Audio")]
     [SerializeField] private AudioSource audioSource;
@@ -43,15 +45,33 @@ public class Assistant : MonoBehaviour
         {
             highestStepReached = newValue;
             Debug.Log("Complimenti hai raggiunto lo step: " + highestStepReached);
-        } 
+            StartCoroutine(ShowImageForDuration(true));
+        }
         else
         {
             highestStepReached = newValue;
             Debug.Log("Oh noooooooo hai raggiunto lo step: " + highestStepReached);
+            StartCoroutine(ShowImageForDuration(false));
         }
 
         UpdateImage();
         StartCoroutine(PlayStepAudioWithDelay(isSuccess));
+    }
+
+    private IEnumerator ShowImageForDuration(bool isHappy)
+    {
+        if (isHappy)
+        {
+            successImage.enabled = true;  // Mostra l'immagine
+            yield return new WaitForSeconds(2f);  // Aspetta 2 secondi
+            successImage.enabled = false;  // Nascondi l'immagine
+        }
+        else
+        {
+            failImage.enabled = true;  // Mostra l'immagine
+            yield return new WaitForSeconds(2f);  // Aspetta 2 secondi
+            failImage.enabled = false;  // Nascondi l'immagine
+        }
     }
 
     private void UpdateImage()
